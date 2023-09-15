8d7a78e0062dfbfe6ad38ce3c915a7e5fea09e96
Minoru Sakakibara <qqgm7z69@gmail.com>
2023-09-16 08:34:58 +0900

    アルファベット大小、数字、記号が必ず含まれるようにスクリプトを変更

--- js/script.js
+++ js/script.js
@@ -1,15 +1,45 @@
-let wordlist = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789@!#$%&';
+let character = 'abcdefghijklmnopqrstuvwxyz';
+let bigCharacter = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+let number = '0123456789';
+let symbol = '@!#$%&';
 
 function createPassword() {
-    let leng = 10;
+    let characterLeng = 3;
+    let bigCharacterLeng = 3;
+    let numberLeng = 2;
+    let symbolLeng =2;
+    let characterGroup = '';
+    let bigCharacterGroup = '';
+    let numberGroup = '';
+    let symbolGroup = '';
     let passWord = '';
     
-    for(let i = 0; i < leng; i++){
-        passWord += wordlist.charAt(Math.floor(Math.random() * wordlist.length));
+    for (let i = 0; i < characterLeng; i++) {
+        characterGroup += character.charAt(Math.floor(Math.random() * character.length));
+    };
+
+    for (let i = 0; i < bigCharacterLeng; i++) {
+        bigCharacterGroup += bigCharacter.charAt(Math.floor(Math.random() * bigCharacter.length));
+    };
+
+    for (let i = 0; i < numberLeng; i++) {
+        numberGroup += number.charAt(Math.floor(Math.random() * number.length));
+    };
+
+    for (let i = 0; i < symbolLeng; i++) {
+        symbolGroup += symbol.charAt(Math.floor(Math.random() * symbol.length));
     };
     
+    let passWordBase = characterGroup + bigCharacterGroup + numberGroup + symbolGroup;
+    let passWordAray = passWordBase.split('');
+
+    passWord = passWordAray.sort(function(){
+        return Math.random()-0.5;
+    }).join('');
+
     outPut = document.getElementById("password");
     outPut.innerHTML = passWord
     console.log(passWord);
+    
     return passWord;
 };
